b872ef8752436397956ed6cf9f19f92b3ce4fa5b
Jesús Inurria <j.inurria@gmail.com>
2024-03-06 14:34:50 +0100

    Output for humans and machines

---
+++ src/bin/output-log.rs
@@ -0,0 +1,13 @@
+fn main(){}
+/**
+ * Debería de salir esto
+ * 
+ * $ $env:RUST_LOG="info"
+$ cargo run --bin output-log
+    Finished dev [unoptimized + debuginfo] target(s) in 0.17s
+     Running `target/debug/output-log.exe`
+[2018-11-30T20:25:52Z INFO  output_log] starting up
+[2018-11-30T20:25:52Z WARN  output_log] oops, nothing implemented!
+
+ * 
+ */
---
+++ src/output_humans_machines.rs
@@ -0,0 +1,7 @@
+use log::{info, warn};
+use env_logger;
+fn main() {
+    env_logger::init();
+    info!("starting up");
+    warn!("oops, nothing implemented!");
+}
