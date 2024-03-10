ff5bc571ac08760563175434a53f7c3db912aa99
Jesús Inurria <j.inurria@gmail.com>
2024-03-10 13:06:03 +0100

    Empiezo señales, quizás tenga problemas por que estoy en windows

---
+++ src/ctrl_signal.rs
@@ -0,0 +1,14 @@
+
+use std::{thread, time::Duration};
+use ctrlc;
+
+fn ctrlc_signal() {
+    ctrlc::set_handler(move || {
+        println!("received Ctrl+C!");
+    })
+        .expect("Error setting Ctrl-C handler");
+
+    // Following code does the actual work, and can be interrupted by pressing
+    // Ctrl-C. As an example: Let's wait a few seconds.
+    thread::sleep(Duration::from_secs(2));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-mod find_matches;
+use signal_hook::{consts::SIGINT, iterator::Signals};
+use std::{error::Error, thread, time::Duration};
 
-fn main(){}
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut signals = Signals::new(&[SIGINT])?;
+
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            println!("Received signal {:?}", sig);
+        }
+    });
+
+    // Following code does the actual work, and can be interrupted by pressing
+    // Ctrl-C. As an example: Let's wait a few seconds.
+    thread::sleep(Duration::from_secs(2));
+
+    Ok(())
+}
